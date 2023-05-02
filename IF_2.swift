ce563aff792b3926c30722e2336f578f4bb424ac
Miguel Ángel Mtz Hdz <131746147+AngelMiguel1005@users.noreply.github.com>
2023-05-01 23:54:36 -0600

    Create IF_2.swift

---
+++ IF_2.swift
@@ -0,0 +1,23 @@
+let myNumber = -10
+
+//Operadores condicionales:
+// ==
+// >
+// <
+// >=
+// <=
+// !=
+
+//Operadores lógicos
+// && AND
+// || OR
+
+if (myNumber < 10 && myNumber > 5) || myNumber >= 50{
+  print("\(myNumber) es mayor que 5 y menor que 10; o es mayor o igual que 50")
+} else if myNumber == 1 {
+    print("\(myNumber) es igual a 1")
+} else if myNumber == -10 {
+    print("\(myNumber) es igual a -10")
+} else {
+  print("\(myNumber) es menor o igual que 5, y es mayor o igual que 10, o es mayor que 50")
+}
