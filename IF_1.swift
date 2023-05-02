94e3fcd03f46657abda882d1e65d4208ecad8713
Miguel Ángel Mtz Hdz <131746147+AngelMiguel1005@users.noreply.github.com>
2023-05-01 23:53:57 -0600

    Create IF_1.swift

---
+++ IF_1.swift
@@ -0,0 +1,15 @@
+let myNumber = 12
+
+//Operadores condicionales:
+// ==
+// >
+// <
+// >=
+// <=
+// !=
+
+if myNumber < 10 {
+  print("\(myNumber) es menor que 10")
+} else {
+  print("\(myNumber) es igual o mayor que 10")
+}
